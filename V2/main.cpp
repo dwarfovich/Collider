43d739d22816723679d3f957d6aba2f68127eca0
Tim Gilevich <ouncetim@gmail.com>
2021-07-27 01:07:34 +0300

    wip

--- V2/main.cpp
+++ V2/main.cpp
@@ -4,15 +4,42 @@ struct A;
 struct B;
 struct C;
 
-void collide(A const& , A const& ) { std::cout << "(A, A)\n"; }
-void collide(B const& , B const& ) { std::cout << "(B, B)\n"; }
-void collide(C const& , C const& ) { std::cout << "(C, C)\n"; }
-void collide(A const& , B const& ) { std::cout << "(A, B)\n"; }
-void collide(B const& , A const& ) { std::cout << "(B, A)\n"; }
-void collide(A const& , C const& ) { std::cout << "(A, C)\n"; }
-void collide(C const& , A const& ) { std::cout << "(C, A)\n"; }
-void collide(C const& , B const& ) { std::cout << "(C, B)\n"; }
-void collide(B const& , C const& ) { std::cout << "(B, C)\n"; }
+void collide(A const&, A const&)
+{
+    std::cout << "(A, A)\n";
+}
+void collide(B const&, B const&)
+{
+    std::cout << "(B, B)\n";
+}
+void collide(C const&, C const&)
+{
+    std::cout << "(C, C)\n";
+}
+void collide(A const&, B const&)
+{
+    std::cout << "(A, B)\n";
+}
+void collide(B const&, A const&)
+{
+    std::cout << "(B, A)\n";
+}
+void collide(A const&, C const&)
+{
+    std::cout << "(A, C)\n";
+}
+void collide(C const&, A const&)
+{
+    std::cout << "(C, A)\n";
+}
+void collide(C const&, B const&)
+{
+    std::cout << "(C, B)\n";
+}
+void collide(B const&, C const&)
+{
+    std::cout << "(B, C)\n";
+}
 
 struct CollisionDispatcherBase
 {
@@ -24,7 +51,7 @@ struct CollisionDispatcherBase
 template<class T>
 struct CollisionDispatcher : CollisionDispatcherBase
 {
-    CollisionDispatcher(T const& t): t(t) { }
+    CollisionDispatcher(T const& t) : t(t) {}
 
     T const& t;
 
@@ -33,29 +60,41 @@ struct CollisionDispatcher : CollisionDispatcherBase
     void collide(C const& other) const override { ::collide(t, other); };
 };
 
-struct Shape
+struct Base
 {
-    virtual ~Shape() {}
+    virtual ~Base() {}
 
-    virtual void visit(Shape const& other) const = 0;
+    virtual void visit(Base const& other) const                = 0;
     virtual void visit(CollisionDispatcherBase const& c) const = 0;
 };
 
-struct A : Shape
+struct A : Base
 {
-    void visit(Shape const& other) const override { auto c = CollisionDispatcher<A>{ *this }; other.visit(c); }
+    void visit(Base const& other) const override
+    {
+        auto c = CollisionDispatcher<A> { *this };
+        other.visit(c);
+    }
     void visit(CollisionDispatcherBase const& c) const override { c.collide(*this); }
 };
 
-struct B : Shape
+struct B : Base
 {
-    void visit(Shape const& other) const override { auto c = CollisionDispatcher<B>{ *this }; other.visit(c); }
+    void visit(Base const& other) const override
+    {
+        auto c = CollisionDispatcher<B> { *this };
+        other.visit(c);
+    }
     void visit(CollisionDispatcherBase const& c) const override { c.collide(*this); }
 };
 
-struct C : Shape
+struct C : Base
 {
-    void visit(Shape const& other) const override { auto c = CollisionDispatcher<C>{ *this }; other.visit(c); }
+    void visit(Base const& other) const override
+    {
+        auto c = CollisionDispatcher<C> { *this };
+        other.visit(c);
+    }
     void visit(CollisionDispatcherBase const& c) const override { c.collide(*this); }
 };
 
@@ -64,12 +103,12 @@ struct C : Shape
 
 int main()
 {
-    auto colliders = std::vector<std::unique_ptr<Shape>>();
-    colliders.push_back(std::make_unique<A>());
-    colliders.push_back(std::make_unique<B>());
-    colliders.push_back(std::make_unique<C>());
+    auto collidees = std::vector<std::unique_ptr<Base>>();
+    collidees.push_back(std::make_unique<A>());
+    collidees.push_back(std::make_unique<B>());
+    collidees.push_back(std::make_unique<C>());
 
-    for (auto i = std::size_t{ 0 }; i != colliders.size() - 1; ++i)
-        for (auto j = i + 1; j != colliders.size(); ++j)
-            colliders[i]->visit(*colliders[j]);
+    for (auto i = std::size_t { 0 }; i != collidees.size() - 1; ++i)
+        for (auto j = i + 1; j != collidees.size(); ++j)
+            collidees[i]->visit(*collidees[j]);
 }
